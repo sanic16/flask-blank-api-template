42e559b7248efb2ec4218a3c2fe1d9a0f7a8e60d
julio <julio.sanic.gt.256@gmail.com>
2024-02-16 19:26:38 -0600

    user model agregado

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 from flask import Flask
+from flask_migrate import Migrate
 
-app = Flask(__name__)
+from extensions import db, jwt
 
-@app.route('/')
-def index():
-    return 'Hello, World!'
+from flask_restful import Api
+
+from config import Config
+
+from models.user import User
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    register_extensions(app=app)
+    register_resources(app=app)
+    return app
+
+
+def register_extensions(app):
+    db.init_app(app=app)
+    jwt.init_app(app=app)
+    migrate = Migrate(app=app, db=db)
+
+def register_resources(app):
+    api = Api(app=app)    
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models/user.py
@@ -0,0 +1,32 @@
+from extensions import db
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    email = db.Column(db.String(75), nullable=False, unique=True)
+    password = db.Column(db.String(32))
+    gender = db.Column(db.String(10))
+    birthDate = db.Column(db.Date)
+    firstName = db.Column(db.String(50))
+    lastName = db.Column(db.String(50))
+    department = db.Column(db.String(25))
+    municipality = db.Column(db.String(25))
+    address = db.Column(db.String(100))
+    phoneNumber = db.Column(db.Integer)
+    
+    is_active = db.Column(db.Boolean, default=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    created_at = db.Column(
+        db.DateTime(), 
+        nullable=False,
+        server_default=db.func.now()
+    )
+    updated_at = db.Column(
+        db.DateTime(),
+        nullable=False,
+        server_default=db.func.now(),
+        onupdate=db.func.now()
+    )
+    
